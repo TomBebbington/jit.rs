42ceb71c40557d7a3b5c0785e3259272ac0021f3
Tom Bebbington <tophattedcoder@gmail.com>
2014-06-20 22:27:46 +0100

    Change all tabs to spaced to conform to official style guide

--- src/libjit/compile.rs
+++ src/libjit/compile.rs
@@ -1,299 +1,299 @@
 use bindings::{
-	jit_nint,
+    jit_nint,
 
-	jit_type_void_ptr,
-	jit_type_ubyte,
-	jit_type_sbyte,
-	jit_type_ushort,
-	jit_type_short,
-	jit_type_uint,
-	jit_type_int,
-	jit_type_nuint,
-	jit_type_nint,
-	jit_type_float32,
-	jit_type_float64,
-	jit_type_sys_bool,
-	jit_type_sys_char,
+    jit_type_void_ptr,
+    jit_type_ubyte,
+    jit_type_sbyte,
+    jit_type_ushort,
+    jit_type_short,
+    jit_type_uint,
+    jit_type_int,
+    jit_type_nuint,
+    jit_type_nint,
+    jit_type_float32,
+    jit_type_float64,
+    jit_type_sys_bool,
+    jit_type_sys_char,
 
-	jit_value_create_long_constant,
-	jit_value_create_nint_constant,
-	jit_value_create_float64_constant,
-	jit_value_create_float32_constant
+    jit_value_create_long_constant,
+    jit_value_create_nint_constant,
+    jit_value_create_float64_constant,
+    jit_value_create_float32_constant
 };
 use function::{
-	CDECL,
-	Function
+    CDECL,
+    Function
 };
 use value::Value;
 use types::{
-	Type,
-	TypeKind,
-	Void,
-	Float32,
-	Float64,
-	NInt,
-	NUInt,
-	Int,
-	UInt,
-	Short,
-	UShort,
-	SByte,
-	UByte,
-	SysBool,
-	SysChar
+    Type,
+    TypeKind,
+    Void,
+    Float32,
+    Float64,
+    NInt,
+    NUInt,
+    Int,
+    UInt,
+    Short,
+    UShort,
+    SByte,
+    UByte,
+    SysBool,
+    SysChar
 };
 use util::NativeRef;
 use std::default::Default;
 /// A type that can be compiled into a LibJIT representation
 pub trait Compile {
-	/// Get a JIT representation of this value
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a>;
-	/// Get the JIT type repr of the value
-	fn jit_type(_:Option<Self>) -> Type;
+    /// Get a JIT representation of this value
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a>;
+    /// Get the JIT type repr of the value
+    fn jit_type(_:Option<Self>) -> Type;
 }
 impl Compile for () {
-	fn compile(&self, func:&Function) -> Value {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_void_ptr, 0))
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<()>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_void_ptr)
-		}
-	}
+    fn compile(&self, func:&Function) -> Value {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_void_ptr, 0))
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<()>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_void_ptr)
+        }
+    }
 }
 impl Compile for f64 {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_float64_constant(func.as_ptr(), jit_type_float64, *self) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<f64>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_float64)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_float64_constant(func.as_ptr(), jit_type_float64, *self) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<f64>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_float64)
+        }
+    }
 }
 impl Compile for f32 {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_float32_constant(func.as_ptr(), jit_type_float32, *self) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<f32>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_float32)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_float32_constant(func.as_ptr(), jit_type_float32, *self) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<f32>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_float32)
+        }
+    }
 }
 impl Compile for int {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_long_constant(func.as_ptr(), jit_type_nint, *self as i64) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<int>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_nint)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_long_constant(func.as_ptr(), jit_type_nint, *self as i64) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<int>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_nint)
+        }
+    }
 }
 impl Compile for uint {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_nuint, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<uint>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_nuint)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_nuint, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<uint>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_nuint)
+        }
+    }
 }
 impl Compile for i32 {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_int, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<i32>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_int)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_int, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<i32>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_int)
+        }
+    }
 }
 impl Compile for u32 {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_uint, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<u32>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_uint)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_uint, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<u32>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_uint)
+        }
+    }
 }
 impl Compile for i16 {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_short, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<i16>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_short)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_short, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<i16>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_short)
+        }
+    }
 }
 impl Compile for u16 {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_ushort, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<u16>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_ushort)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_ushort, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<u16>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_ushort)
+        }
+    }
 }
 impl Compile for i8 {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_sbyte, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<i8>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_sbyte)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_sbyte, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<i8>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_sbyte)
+        }
+    }
 }
 impl Compile for u8 {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_ubyte, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<u8>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_ubyte)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_ubyte, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<u8>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_ubyte)
+        }
+    }
 }
 impl Compile for bool {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_sys_bool, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<bool>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_sys_bool)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_sys_bool, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<bool>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_sys_bool)
+        }
+    }
 }
 impl Compile for char {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_sys_char, *self as jit_nint) )
-		}
-	}
-	#[inline]
-	fn jit_type(_:Option<char>) -> Type {
-		unsafe {
-			NativeRef::from_ptr(jit_type_sys_char)
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_create_nint_constant(func.as_ptr(), jit_type_sys_char, *self as jit_nint) )
+        }
+    }
+    #[inline]
+    fn jit_type(_:Option<char>) -> Type {
+        unsafe {
+            NativeRef::from_ptr(jit_type_sys_char)
+        }
+    }
 }
 impl<'t> Compile for &'t str {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		let cstring_t = Compile::jit_type(None::<&'t str>);
-		let strlen_i = (self.len() as i32).compile(func);
-		let bufptr = Value::new(func, &cstring_t);
-		func.insn_store(&bufptr, &func.insn_alloca(&strlen_i));
-		for i in range(0, self.len()) {
-			let char_i = self.char_at(i).compile(func);
-			func.insn_store_relative(&bufptr, i as int, &char_i);
-		}
-		let null_term = '\0'.compile(func);
-		func.insn_store_relative(&bufptr, self.len() as int, &null_term);
-		bufptr
-	}
-	#[inline]
-	fn jit_type(_:Option<&'t str>) -> Type {
-		unsafe {
-			Type::create_pointer(&NativeRef::from_ptr(jit_type_sys_char))
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        let cstring_t = Compile::jit_type(None::<&'t str>);
+        let strlen_i = (self.len() as i32).compile(func);
+        let bufptr = Value::new(func, &cstring_t);
+        func.insn_store(&bufptr, &func.insn_alloca(&strlen_i));
+        for i in range(0, self.len()) {
+            let char_i = self.char_at(i).compile(func);
+            func.insn_store_relative(&bufptr, i as int, &char_i);
+        }
+        let null_term = '\0'.compile(func);
+        func.insn_store_relative(&bufptr, self.len() as int, &null_term);
+        bufptr
+    }
+    #[inline]
+    fn jit_type(_:Option<&'t str>) -> Type {
+        unsafe {
+            Type::create_pointer(&NativeRef::from_ptr(jit_type_sys_char))
+        }
+    }
 }
 impl Compile for String {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		self.as_slice().compile(func)
-	}
-	#[inline]
-	fn jit_type(_:Option<String>) -> Type {
-		unsafe {
-			Type::create_pointer(&NativeRef::from_ptr(jit_type_sys_char))
-		}
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        self.as_slice().compile(func)
+    }
+    #[inline]
+    fn jit_type(_:Option<String>) -> Type {
+        unsafe {
+            Type::create_pointer(&NativeRef::from_ptr(jit_type_sys_char))
+        }
+    }
 }
 impl<T:Compile> Compile for *T {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		func.insn_convert(&self.to_uint().compile(func), &::get_type::<T>(), false)
-	}
-	#[inline]
-	fn jit_type(_:Option<*T>) -> Type {
-		Type::create_pointer(&::get_type::<T>())
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        func.insn_convert(&self.to_uint().compile(func), &::get_type::<T>(), false)
+    }
+    #[inline]
+    fn jit_type(_:Option<*T>) -> Type {
+        Type::create_pointer(&::get_type::<T>())
+    }
 }
 impl<R:Compile> Compile for fn() -> R {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		func.insn_convert(&(self as *fn() -> R).to_uint().compile(func), &::get_type::<fn() -> R>(), false)
-	}
-	#[inline]
-	fn jit_type(_:Option<fn() -> R>) -> Type {
-		Type::create_signature(CDECL, &::get_type::<R>(), &mut [])
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        func.insn_convert(&(self as *fn() -> R).to_uint().compile(func), &::get_type::<fn() -> R>(), false)
+    }
+    #[inline]
+    fn jit_type(_:Option<fn() -> R>) -> Type {
+        Type::create_signature(CDECL, &::get_type::<R>(), &mut [])
+    }
 }
 impl<A:Compile, R:Compile> Compile for fn(A) -> R {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		func.insn_convert(&(self as *fn(A) -> R).to_uint().compile(func), &::get_type::<fn(A) -> R>(), false)
-	}
-	#[inline]
-	fn jit_type(_:Option<fn(A) -> R>) -> Type {
-		Type::create_signature(CDECL, &::get_type::<R>(), &mut [&::get_type::<A>()])
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        func.insn_convert(&(self as *fn(A) -> R).to_uint().compile(func), &::get_type::<fn(A) -> R>(), false)
+    }
+    #[inline]
+    fn jit_type(_:Option<fn(A) -> R>) -> Type {
+        Type::create_signature(CDECL, &::get_type::<R>(), &mut [&::get_type::<A>()])
+    }
 }
 impl<A:Compile, B:Compile, R:Compile> Compile for fn(A, B) -> R {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		func.insn_convert(&(self as *fn(A, B) -> R).to_uint().compile(func), &::get_type::<fn(A, B) -> R>(), false)
-	}
-	#[inline]
-	fn jit_type(_:Option<fn(A, B) -> R>) -> Type {
-		Type::create_signature(CDECL, &::get_type::<R>(), &mut [&::get_type::<A>(), &::get_type::<B>()])
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        func.insn_convert(&(self as *fn(A, B) -> R).to_uint().compile(func), &::get_type::<fn(A, B) -> R>(), false)
+    }
+    #[inline]
+    fn jit_type(_:Option<fn(A, B) -> R>) -> Type {
+        Type::create_signature(CDECL, &::get_type::<R>(), &mut [&::get_type::<A>(), &::get_type::<B>()])
+    }
 }
 impl<A:Compile, B:Compile, C:Compile, R:Compile> Compile for fn(A, B, C) -> R {
-	fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
-		func.insn_convert(&(self as *fn(A, B, C) -> R).to_uint().compile(func), &::get_type::<fn(A, B, C) -> R>(), false)
-	}
-	#[inline]
-	fn jit_type(_:Option<fn(A, B, C) -> R>) -> Type {
-		Type::create_signature(CDECL, &::get_type::<R>(), &mut [&::get_type::<A>(), &::get_type::<B>(), &::get_type::<C>()])
-	}
+    fn compile<'a>(&self, func:&Function<'a>) -> Value<'a> {
+        func.insn_convert(&(self as *fn(A, B, C) -> R).to_uint().compile(func), &::get_type::<fn(A, B, C) -> R>(), false)
+    }
+    #[inline]
+    fn jit_type(_:Option<fn(A, B, C) -> R>) -> Type {
+        Type::create_signature(CDECL, &::get_type::<R>(), &mut [&::get_type::<A>(), &::get_type::<B>(), &::get_type::<C>()])
+    }
 }
--- src/libjit/value.rs
+++ src/libjit/value.rs
@@ -1,17 +1,17 @@
 use bindings::{
-	jit_value_t,
-	jit_value_create,
-	jit_value_is_addressable,
-	jit_value_is_temporary,
-	jit_value_get_context,
-	jit_value_get_type,
-	jit_value_get_function,
-	jit_value_set_addressable
+    jit_value_t,
+    jit_value_create,
+    jit_value_is_addressable,
+    jit_value_is_temporary,
+    jit_value_get_context,
+    jit_value_get_type,
+    jit_value_get_function,
+    jit_value_set_addressable
 };
 use compile::Compile;
 use context::{
-	Context,
-	InContext
+    Context,
+    InContext
 };
 use function::Function;
 use std::kinds::marker::ContravariantLifetime;
@@ -27,157 +27,157 @@ use util::NativeRef;
 */
 #[deriving(Clone, PartialEq)]
 pub struct Value<'a> {
-	_value: jit_value_t,
-	marker: ContravariantLifetime<'a>
+    _value: jit_value_t,
+    marker: ContravariantLifetime<'a>
 }
 impl<'a> NativeRef for Value<'a> {
-	#[inline]
-	/// Convert to a native pointer
-	unsafe fn as_ptr(&self) -> jit_value_t {
-		self._value
-	}
-	#[inline]
-	/// Convert from a native pointer
-	unsafe fn from_ptr(ptr:jit_value_t) -> Value {
-		Value {
-			_value: ptr,
-			marker: ContravariantLifetime::<'a>
-		}
-	}
+    #[inline]
+    /// Convert to a native pointer
+    unsafe fn as_ptr(&self) -> jit_value_t {
+        self._value
+    }
+    #[inline]
+    /// Convert from a native pointer
+    unsafe fn from_ptr(ptr:jit_value_t) -> Value {
+        Value {
+            _value: ptr,
+            marker: ContravariantLifetime::<'a>
+        }
+    }
 }
 impl<'a> InContext<'a> for Value<'a> {
-	/// Get the context which this value belongs to
-	fn get_context(&self) -> Context<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_get_context(self.as_ptr()))
-		}
-	}
+    /// Get the context which this value belongs to
+    fn get_context(&self) -> Context<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_get_context(self.as_ptr()))
+        }
+    }
 }
 impl<'a> Value<'a> {
-	/**
-	 * Create a new value in the context of a function's current block.
-	 * The value initially starts off as a block-specific temporary.
-	 * It will be converted into a function-wide local variable if
-	 * it is ever referenced from a different block.
-	 */
-	pub fn new(func:&Function<'a>, value_type:&Type) -> Value<'a> {
-		unsafe {
-			let value = jit_value_create(func.as_ptr(), value_type.as_ptr());
-			NativeRef::from_ptr(value)
-		}
-	}
-	/// Get the type of the value
-	pub fn get_type(&self) -> Type {
-		unsafe {
-			let ty = jit_value_get_type(self.as_ptr());
-			NativeRef::from_ptr(ty)
-		}
-	}
-	/// Get the function which owns this value
-	#[inline]
-	pub fn get_function(&self) -> Function<'a> {
-		unsafe {
-			NativeRef::from_ptr(jit_value_get_function(self.as_ptr()))
-		}
-	}
-	/**
-	 * Determine if a value is temporary.  i.e. its scope extends
-	 * over a single block within its function.
-	 */
-	#[inline]
-	pub fn is_temp(&self) -> bool {
-		unsafe {
-			jit_value_is_temporary(self.as_ptr()) != 0
-		}
-	}
-	/// Determine if a value is addressable.
-	#[inline]
-	pub fn is_addressable(&self) -> bool {
-		unsafe {
-			jit_value_is_addressable(self.as_ptr()) != 0
-		}
-	}
-	/* Set a flag on a value to indicate that it is addressable.
-	 * This should be used when you want to take the address of a
-	 * value (e.g. `&variable` in Rust/C).  The value is guaranteed
-	 * to not be stored in a register across a function call.
-	 */
-	#[inline]
-	pub fn set_addressable(&self) -> () {
-		unsafe {
-			jit_value_set_addressable(self.as_ptr())
-		}
-	}
+    /**
+     * Create a new value in the context of a function's current block.
+     * The value initially starts off as a block-specific temporary.
+     * It will be converted into a function-wide local variable if
+     * it is ever referenced from a different block.
+     */
+    pub fn new(func:&Function<'a>, value_type:&Type) -> Value<'a> {
+        unsafe {
+            let value = jit_value_create(func.as_ptr(), value_type.as_ptr());
+            NativeRef::from_ptr(value)
+        }
+    }
+    /// Get the type of the value
+    pub fn get_type(&self) -> Type {
+        unsafe {
+            let ty = jit_value_get_type(self.as_ptr());
+            NativeRef::from_ptr(ty)
+        }
+    }
+    /// Get the function which owns this value
+    #[inline]
+    pub fn get_function(&self) -> Function<'a> {
+        unsafe {
+            NativeRef::from_ptr(jit_value_get_function(self.as_ptr()))
+        }
+    }
+    /**
+     * Determine if a value is temporary.  i.e. its scope extends
+     * over a single block within its function.
+     */
+    #[inline]
+    pub fn is_temp(&self) -> bool {
+        unsafe {
+            jit_value_is_temporary(self.as_ptr()) != 0
+        }
+    }
+    /// Determine if a value is addressable.
+    #[inline]
+    pub fn is_addressable(&self) -> bool {
+        unsafe {
+            jit_value_is_addressable(self.as_ptr()) != 0
+        }
+    }
+    /* Set a flag on a value to indicate that it is addressable.
+     * This should be used when you want to take the address of a
+     * value (e.g. `&variable` in Rust/C).  The value is guaranteed
+     * to not be stored in a register across a function call.
+     */
+    #[inline]
+    pub fn set_addressable(&self) -> () {
+        unsafe {
+            jit_value_set_addressable(self.as_ptr())
+        }
+    }
 }
 impl<'a> Add<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn add(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_add(self, other)
-	}
+    #[inline(always)]
+    fn add(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_add(self, other)
+    }
 }
 impl<'a> Sub<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn sub(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_sub(self, other)
-	}
+    #[inline(always)]
+    fn sub(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_sub(self, other)
+    }
 }
 impl<'a> Mul<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn mul(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_mul(self, other)
-	}
+    #[inline(always)]
+    fn mul(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_mul(self, other)
+    }
 }
 impl<'a> Div<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn div(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_div(self, other)
-	}
+    #[inline(always)]
+    fn div(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_div(self, other)
+    }
 }
 impl<'a> Rem<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn rem(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_rem(self, other)
-	}
+    #[inline(always)]
+    fn rem(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_rem(self, other)
+    }
 }
 impl<'a> Shl<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn shl(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_shl(self, other)
-	}
+    #[inline(always)]
+    fn shl(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_shl(self, other)
+    }
 }
 impl<'a> Shr<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn shr(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_shr(self, other)
-	}
+    #[inline(always)]
+    fn shr(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_shr(self, other)
+    }
 }
 impl<'a> BitAnd<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn bitand(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_and(self, other)
-	}
+    #[inline(always)]
+    fn bitand(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_and(self, other)
+    }
 }
 impl<'a> BitOr<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn bitor(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_or(self, other)
-	}
+    #[inline(always)]
+    fn bitor(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_or(self, other)
+    }
 }
 impl<'a> BitXor<Value<'a>, Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn bitxor(&self, other:&Value<'a>) -> Value<'a> {
-		self.get_function().insn_xor(self, other)
-	}
+    #[inline(always)]
+    fn bitxor(&self, other:&Value<'a>) -> Value<'a> {
+        self.get_function().insn_xor(self, other)
+    }
 }
 impl<'a> Neg<Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn neg(&self) -> Value<'a> {
-		self.get_function().insn_neg(self)
-	}
+    #[inline(always)]
+    fn neg(&self) -> Value<'a> {
+        self.get_function().insn_neg(self)
+    }
 }
 impl<'a> Not<Value<'a>> for Value<'a> {
-	#[inline(always)]
-	fn not(&self) -> Value<'a> {
-		self.get_function().insn_not(self)
-	}
+    #[inline(always)]
+    fn not(&self) -> Value<'a> {
+        self.get_function().insn_not(self)
+    }
 }
