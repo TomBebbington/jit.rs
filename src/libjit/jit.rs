b3f1db63f3c0449732ee0f0880ad45f6d7692339
Tom Bebbington <tophattedcoder@gmail.com>
2014-06-23 21:20:38 +0100

    Add `unsafe_destructor` as feature

--- src/libjit/jit.rs
+++ src/libjit/jit.rs
@@ -29,7 +29,7 @@
 #![crate_type = "rlib"]
 #![allow(raw_pointer_deriving, dead_code, non_camel_case_types)]
 #![deny(unnecessary_parens, unrecognized_lint, unreachable_code, unnecessary_allocation, unnecessary_typecast, unnecessary_allocation, uppercase_variables, unused_must_use)]
-#![feature(globs, macro_rules)]
+#![feature(unsafe_destructor, globs, macro_rules)]
 #![stable]
 //! This crate wraps LibJIT in an idiomatic style.
 //! For example, here's a quick example which makes a multiply function using LibJIT:
