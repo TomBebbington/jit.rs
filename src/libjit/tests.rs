3eaba39fffb11d315bc22e8b07d59a3b1b4bfe86
Tom Bebbington <tophattedcoder@gmail.com>
2014-07-13 13:59:06 +0100

    Update tests

--- src/libjit/tests.rs
+++ src/libjit/tests.rs
@@ -1,13 +1,10 @@
 use compile::Compile;
 use context::Context;
 use function::{CDECL, Function};
-use label::Label;
 use std::default::Default;
-use std::io::stdio::println;
 use test::Bencher;
 use types::*;
 use get_type = types::get;
-static BENCH_SIZE: uint = 200000;
 fn with_empty_func(cb:|&Context, &Function| -> ()) -> () {
     let ctx = Context::new();
     ctx.build(|| {
@@ -16,13 +13,18 @@ fn with_empty_func(cb:|&Context, &Function| -> ()) -> () {
         cb(&ctx, &func)
     })
 }
-fn test_compile<T:Compile+Default>(kind:TypeKind) {
-    with_empty_func(|_, func| {
-        let pval:T = Default::default();
-        let val = pval.compile(func);
-        assert!(val.get_type().get_kind().contains(kind));
-    })
-}
+macro_rules! test_compile(
+    ($ty:ty, $test_name:ident, $kind:expr) => (
+        #[test]
+        fn $test_name() {
+            with_empty_func(|_, func| {
+                let pval:$ty = Default::default();
+                let val = pval.compile(func);
+                assert_eq!(val.get_type().get_kind(), $kind);
+            })
+        }
+    );
+)
 #[test]
 fn test_sqrt() {
     let sig = get::<fn(uint) -> uint>();
@@ -39,7 +41,9 @@ fn test_sqrt() {
         assert_eq!(sqrt(9), 3);
         assert_eq!(sqrt(4), 2);
         assert_eq!(sqrt(1), 1);
-    })
+    });
+    let result:uint = func.apply1(64u);
+    assert_eq!(result, 8u);
 }
 #[test]
 fn test_struct() {
@@ -48,14 +52,14 @@ fn test_struct() {
         "first": f64,
         "second": f64
     });
-    assert_eq!(double_float_t.find_name("first"), 0);
-    assert_eq!(double_float_t.find_name("second"), 1);
-    let fields:Vec<String> = double_float_t.iter_fields().map(|field| field.get_name().unwrap()).collect();
+    assert_eq!(double_float_t.find_name("first").index, 0);
+    assert_eq!(double_float_t.find_name("second").index, 1);
+    let fields:Vec<String> = double_float_t.fields().map(|field| field.get_name().unwrap()).collect();
     assert!(fields.as_slice() == [
         "first".into_string(),
         "second".into_string()
     ].as_slice())
-    let mut iter = double_float_t.iter_fields();
+    let mut iter = double_float_t.fields();
     let (size, _) = iter.size_hint();
     assert_eq!(size, 2);
     assert!({
@@ -64,59 +68,16 @@ fn test_struct() {
     });
     assert_eq!(iter.count(), 0);
 }
-#[test]
-fn test_compiles() {
-    test_compile::<()>(Void);
-    test_compile::<f64>(Float64);
-    test_compile::<f32>(Float32);
-    test_compile::<int>(NInt);
-    test_compile::<uint>(NUInt);
-    test_compile::<i32>(Int);
-    test_compile::<u32>(UInt);
-    test_compile::<i16>(Short);
-    test_compile::<u16>(UShort);
-    test_compile::<i8>(SByte);
-    test_compile::<u8>(UByte);
-    test_compile::<bool>(SysBool);
-    test_compile::<char>(SysChar);
-}
-#[bench]
-fn bench_native_fib(b: &mut Bencher) {
-    fn fib(n: uint) -> uint {
-        match n {
-            0     => 0,
-            1 | 2 => 1,
-            3     => 2,
-            _     => fib(n - 1) + fib(n - 2)
-        }
-    }
-    b.iter(|| {
-        fib(BENCH_SIZE);
-    });
-}
-#[bench]
-fn bench_jit_fib(b: &mut Bencher) {
-    let context = Context::new();
-    let sig = get::<fn(uint) -> uint>();
-    let fib = Function::new(&context, sig.clone());
-    let n = fib.get_param(0);
-    println("Making jump table");
-    jit!(&fib, jump_table(&n, zero, one, two, three));
-    println("n - 1");
-    let n_m1 = n - jit!(fib, 1u);
-    println("fib_m1");
-    let fib_m1 = jit!(fib, call(&fib, &n_m1));
-    println("fib_m2");
-    let n_m2 = n - jit!(fib, 2u);
-    let fib_m2 = jit!(fib, call(&fib, &n_m2));
-    println("Adding");
-    let result = fib_m1 + fib_m2;
-    println("Returning");
-    jit!(fib, return &result);
-    fib.compile();
-    fib.with_closure1(|fib:fn(uint) -> uint| {
-        b.iter(|| {
-            fib(BENCH_SIZE);
-        });
-    });
-}
+test_compile!((), test_compile_void, Void)
+test_compile!(f64, test_compile_f64, Float64)
+test_compile!(f32, test_compile_f32, Float32)
+test_compile!(int, test_compile_int, NInt)
+test_compile!(uint, test_compile_uint, NUInt)
+test_compile!(i32, test_compile_i32, Int)
+test_compile!(u32, test_compile_u32, UInt)
+test_compile!(i16, test_compile_i16, Short)
+test_compile!(u16, test_compile_u16, UShort)
+test_compile!(i8, test_compile_i8, SByte)
+test_compile!(u8, test_compile_u8, UByte)
+test_compile!(bool, test_compile_bool, SysBool)
+test_compile!(char, test_compile_char, SysChar)
