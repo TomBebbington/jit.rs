672f94460be42ea64d16cfdc88af4cacc09e9ec0
Tom Bebbington <tophattedcoder@gmail.com>
2014-11-16 20:40:58 +0000

    Fix build script

--- src/build.rs
+++ src/build.rs
@@ -12,19 +12,22 @@ fn main() {
 	let ref out_path = Path::new(os::getenv("OUT_DIR").unwrap());
 	let ref lib_path = Path::new("/usr/lib/libjit.so");
 	let ref eventual_lib_path = Path::new("native/jit/.libs/libjit.so");
-	let ref dest_path = out_path.join("libjit.so.0");
+	let ref dest_path = out_path.join("libjit.so");
+	let ref submod_path = Path::new("native");
 	if lib_path.exists() {
 		fs::copy(lib_path, dest_path).unwrap();
 	} else if !dest_path.exists() {
 		if !eventual_lib_path.exists() {
 			io::println("No LibJIT found in /usr/lib/ so updating LibJIT");
-			let mut git = Command::new("git");
-			run(git.clone().arg("submodule init"));
-			run(git.arg("submodule update"));
+			if !submod_path.exists() {
+				io::println!("Initialising submodule");
+				run(Command::new("git").arg("submodule").arg("init"));
+			}
+			run(Command::new("git").arg("submodule").arg("update"));
 			let old_cwd = os::getcwd();
-			os::change_dir(&Path::new("native"));
+			os::change_dir(submod_path);
 			io::println("Configuring LibJIT");
-			run(Command::new("sh").arg("autogen.sh"));
+			run(Command::new("sh").arg("auto_gen.sh"));
 			run_cmd("./configure");
 
 			io::println("Building LibJIT");
@@ -39,8 +42,8 @@ fn run_cmd(command: &str) {
 	run(&mut Command::new(command))
 }
 fn run(command: &mut Command) {
-	let process = command.status().unwrap();
-	if !process.success() {
-		panic!("Command '{}' failed", command);
+	let process = command.output().unwrap();
+	if !process.status.success() {
+		panic!("Command '{}' failed with output: \n{}\n{}", command, String::from_utf8_lossy(process.output[]), String::from_utf8_lossy(process.error[]));
 	}
 }
