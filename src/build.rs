ddad01d3b49a56f69174b997c90882b13046df8d
Tom Bebbington <tophattedcoder@gmail.com>
2014-11-17 07:39:30 +0000

    Rename appropriately in build script

--- src/build.rs
+++ src/build.rs
@@ -15,12 +15,12 @@ fn main() {
         Ok(()) => return,
         Err(..) => {}
     }
-	let ref out_path = Path::new(os::getenv("OUT_DIR").unwrap());
-	let ref eventual_lib_path = Path::new("native/jit/.libs/libjit.so");
-	let ref dest_path = out_path.join("libjit.so");
+	let ref out_dir = Path::new(os::getenv("OUT_DIR").unwrap());
+	let ref final_lib_path = Path::new("native/jit/.libs/libjit.so");
+	let ref dest_path = out_dir.join("libjit.so");
 	let ref submod_path = Path::new("native");
 	if !dest_path.exists() {
-		if !eventual_lib_path.exists() {
+		if !final_lib_path.exists() {
 			io::println("No LibJIT found in /usr/lib/ so updating LibJIT");
 			if !submod_path.exists() {
 				io::println("Initialising submodule");
@@ -34,10 +34,11 @@ fn main() {
 			io::println("Building LibJIT");
 			run(Command::new("make").cwd(submod_path));
 		}
-		fs::copy(eventual_lib_path, dest_path).unwrap();
+		fs::copy(final_lib_path, dest_path).unwrap();
 	}
-	println!("cargo:rustc-flags=-L {}", out_path.display());
-    println!("cargo:root={}", out_path.display());
+	println!("jit.rs now at {}", dest_path.display());
+	println!("cargo:rustc-flags=-L {}", out_dir.display());
+    println!("cargo:root={}", out_dir.display());
     println!("cargo:rustc-flags=-l jit:static");
 }
 fn run(command: &mut Command) {
