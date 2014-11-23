5b0063a13d70ae509a74dea23a0d787b2189d504
Tom Bebbington <tophattedcoder@gmail.com>
2014-11-23 23:32:45 +0000

    Add new option to `configure` so LibJIT should have `-fPIC` option enabled

--- src/build.rs
+++ src/build.rs
@@ -3,7 +3,7 @@ extern crate "pkg-config" as pkg_config;
 use std::io;
 use std::io::fs;
 use std::io::fs::PathExtensions;
-use std::io::process::{ProcessExit, Command};
+use std::io::process::Command;
 use std::path::Path;
 use std::os;
 
@@ -14,24 +14,22 @@ fn main() {
 		Err(..) => {}
 	}
 	let ref out_dir = Path::new(os::getenv("OUT_DIR").expect("OUT_DIR needed for compilation"));
-	let ref final_lib_path = Path::new("native/jit/.libs/libjit.so");
-	let ref dest_path = out_dir.join("libjit.so");
+	let ref final_lib_dir = Path::new("native/jit/.libs");
 	let ref submod_path = Path::new("native");
-	if !dest_path.exists() {
-		if !final_lib_path.exists() {
+	if !final_lib_dir.exists() {
+		if !final_lib_dir.exists() {
 			if !submod_path.exists() {
 				run(Command::new("git").arg("submodule").arg("init"));
 			}
 			run(Command::new("git").arg("submodule").arg("update"));
 			run(Command::new("sh").arg("auto_gen.sh").cwd(submod_path));
-			run(Command::new("./configure").cwd(submod_path));
+			run(Command::new("./configure --enable-shared").cwd(submod_path));
 			run(Command::new("make").cwd(submod_path));
 		}
-		fs::copy(final_lib_path, dest_path).unwrap();
 	}
     println!("cargo:root={}", out_dir.display());
     println!("cargo:rustc-flags=-l jit:static");
-	println!("cargo:rustc-flags=-L {}", out_dir.display());
+	println!("cargo:rustc-flags=-L {}", final_lib_dir.display());
 }
 fn run(command: &mut Command) {
 	let process = command.output().unwrap();
