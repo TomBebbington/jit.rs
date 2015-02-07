3d7c4cb977dab97beccbf991e10d03965a71cc6a
Tom Bebbington <tophattedcoder@gmail.com>
2015-02-07 23:43:23 +0000

    Update dependencies and libjit-sys

--- sys/build.rs
+++ sys/build.rs
@@ -1,9 +1,10 @@
-#![feature(io, path, os)]
+#![feature(env, io, path)]
 #[cfg(not(windows))]
 extern crate "pkg-config" as pkg_config;
-use std::old_io::fs::{self, PathExtensions};
 use std::old_io::process::Command;
-use std::os;
+use std::old_io::fs;
+use std::old_path::Path as OPath;
+use std::env;
 use std::path::Path;
 
 #[cfg(windows)]
@@ -14,28 +15,39 @@ static FINAL_LIB:&'static str = "libjit.a";
 
 static MINGW:&'static str = "c:/mingw";
 
+fn exists(path: &Path) -> bool {
+	use std::old_io::fs::{PathExtensions};
+	OPath::new(path.to_str().unwrap()).exists()
+}
+fn path(path: &str) -> &Path {
+	&Path::new(path)
+}
 fn main() {
-	if cfg!(windows) && !Path::new(MINGW).exists() {
+	if cfg!(windows) && !exists(path(MINGW)) {
 		panic!("LibJIT build requires MinGW and MSYS to be installed");
 	} else if pkg_config::find_library("jit").is_ok() {
 		return;
 	}
-    let ref out_dir = Path::new(os::getenv("OUT_DIR").unwrap());
-	let ref submod_path = Path::new("libjit");
-	let ref final_lib_dir = submod_path.join("jit/.libs");
-	if !final_lib_dir.join(FINAL_LIB).exists() {
-		if !submod_path.exists() {
+	let out_dir = env::var("OUT_DIR").unwrap();
+	let out_dir = Path::new(&*out_dir);
+	let submod_path = Path::new("libjit");
+	let final_lib_dir = submod_path.join("jit/.libs");
+	if !exists(&*final_lib_dir.join(FINAL_LIB)) {
+		if !exists(submod_path) {
 			run(Command::new("git").arg("submodule").arg("init"));
 		}
 		run(Command::new("git").arg("submodule").arg("update"));
-		if !submod_path.exists() {
+		if !exists(submod_path) {
 			run(Command::new("git").arg("clone").arg("git://git.savannah.gnu.org/libjit.git").arg(&*submod_path.display().to_string()));
 		}
-		run(Command::new("sh").arg("auto_gen.sh").cwd(submod_path));
-		run(Command::new("sh").arg("configure").arg("--enable-static").arg("--disable-shared").arg("CFLAGS=-fPIC").cwd(submod_path));
-		run(Command::new("make").cwd(submod_path));
+		let submod_path = OPath::new(submod_path.to_str().unwrap());
+		run(Command::new("sh").arg("auto_gen.sh").cwd(&submod_path));
+		run(Command::new("sh").arg("configure").arg("--enable-static").arg("--disable-shared").arg("CFLAGS=-fPIC").cwd(&submod_path));
+		run(Command::new("make").cwd(&submod_path));
 	}
-	fs::copy(&final_lib_dir.join(FINAL_LIB), &out_dir.join(FINAL_LIB)).unwrap();
+	let ref from = OPath::new(final_lib_dir.join(FINAL_LIB).to_str().unwrap());
+	let ref to = OPath::new(out_dir.join(FINAL_LIB).to_str().unwrap());
+	fs::copy(from, to).unwrap();
     println!("cargo:rustc-flags=-l jit:static -L {}", out_dir.display());
 }
 fn run(command: &mut Command) {
