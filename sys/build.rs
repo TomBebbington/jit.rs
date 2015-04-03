d7af6e03eb8bdb63445ec0f055e0c0a7687d0db3
Tom Bebbington <tophattedcoder@gmail.com>
2015-04-03 16:20:20 +0100

    Use more general error message in build script

--- sys/build.rs
+++ sys/build.rs
@@ -16,12 +16,12 @@ static MINGW:&'static str = "c:/mingw";
 
 static INSTALL_AUTOTOOLS_MSG:&'static str = "Failed to generate configuration script. Did you forget to install autotools, bison, flex, and libtool?";
 
-static USE_CARGO_MSG:&'static str = "Build script should be ran with Cargo";
+static USE_CARGO_MSG:&'static str = "Build script should be ran with Cargo, run `cargo build` instead";
 
 #[cfg(windows)]
 static INSTALL_COMPILER_MSG:&'static str = "Failed to configure the library for your platform. Did you forget to install MinGW and MSYS?";
 #[cfg(not(windows))]
-static INSTALL_COMPILER_MSG:&'static str = "Failed to configure the library for your platform. Did you forget to install gcc or clang?";
+static INSTALL_COMPILER_MSG:&'static str = "Failed to configure the library for your platform. Did you forget to install a C compiler?";
 
 fn parse(cmd: &str) -> Command {
 	let mut words = cmd.split(' ');
@@ -42,7 +42,7 @@ fn main() {
 	let out_dir = env::var("OUT_DIR").ok().expect(USE_CARGO_MSG);
 	let num_jobs = env::var("NUM_JOBS").ok().expect(USE_CARGO_MSG);
 	let out_dir = Path::new(&*out_dir);
-	let submod_path = Path::new("libjit");
+    let submod_path = Path::new(&env::var("CARGO_MANIFEST_DIR").ok().expect(USE_CARGO_MSG)).join("libjit");
 	let final_lib_dir = submod_path.join("jit/.libs");
 	if !final_lib_dir.join(FINAL_LIB).exists() {
 		run_wocare(&mut parse("git submodule init"));
@@ -50,8 +50,8 @@ fn main() {
 		if !submod_path.exists() {
 			run(Command::new("git").args(&["clone", "git://git.savannah.gnu.org/libjit.git", submod_path.to_str().unwrap()]))
 		}
-		run_nice(parse("sh auto_gen.sh").current_dir(submod_path), INSTALL_AUTOTOOLS_MSG);
-		run_nice(parse("sh configure --enable-static --disable-shared CC=clang CFLAGS=-fPIC").current_dir(submod_path), INSTALL_COMPILER_MSG);
+		run_nice(parse("sh auto_gen.sh").current_dir(&submod_path), INSTALL_AUTOTOOLS_MSG);
+		run_nice(parse("sh configure --enable-static --disable-shared CFLAGS=-fPIC").current_dir(&submod_path), INSTALL_COMPILER_MSG);
 		run(Command::new("make").arg(&format!("-j{}", num_jobs)).current_dir(submod_path));
 	}
 	let from = final_lib_dir.join(FINAL_LIB);
@@ -59,8 +59,8 @@ fn main() {
 	if let Err(error) = fs::copy(&from, &to) {
 		panic!("Failed to copy library from {:?} to {:?} due to {}", from, to, error)
 	}
-	println!("cargo:rustc-link-lib=jit");
 	println!("cargo:rustc-link-search=native={:?}", out_dir);
+	println!("cargo:rustc-link-lib=static=jit");
 }
 fn run_nice(cmd: &mut Command, text: &str) {
 	if !cmd.status().unwrap().success() {
